7ce75c925cf705f3c8ff31b880fc425b112c24fe
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-21 16:27:31 +0300

    add staticfiles

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+from config import BASE_DIR
 from rest import router
 
+STATIC_PATH = BASE_DIR / "static"
+
 app = FastAPI()
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_PATH),
+    name="static",
+)
 app.include_router(router)
